7021ac4c773397126d8181f4cef7f73a59b1da9d
lochan-DEV <lochan1410a@gmail.com>
2026-07-10 20:30:49 +0530

    Hello,World!

--- hello,world.py
+++ hello,world.py
@@ -4,3 +4,11 @@ def hello():
     return "Hello, World!"
 print(hello())
 
+############################################################################################################################################
+
+# hello() with user input
+
+def hello():
+    x=input("Enter user name: ")
+    return f"Hello, {x}"
+print(hello())
