e0c2398470c4a6eade46d921d1cc0aee7d7b25d7
lochan-DEV <lochan1410a@gmail.com>
2026-07-29 22:16:47 +0530

    Locomotive Engineer

---
+++ Learning_path/locomotive_engineer.py
@@ -0,0 +1,6 @@
+"""Functions which helps the locomotive engineer to keep track of the train."""
+def get_list_of_wagons(*argvs):
+    list = []
+    for i in argvs:
+        list.append(i)
+    return list
