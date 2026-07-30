ad3af04ec3f17e344e4b73f82eeaf06c07854197
lochan-DEV <lochan1410a@gmail.com>
2026-07-30 23:39:47 +0530

    Cater Waiter

--- Learning_path/cater_waiter.py
+++ Learning_path/cater_waiter.py
@@ -18,3 +18,18 @@ def check_drinks(drink_name, drink_ingredients):
         return drink_name + " Cocktail"
     else:
         return drink_name + " Mocktail"
+
+
+def categorize_dish(dish_name, dish_ingredients):
+    dish_ingredients = set(dish_ingredients)
+    if dish_ingredients <= set(VEGAN):
+        return f"{dish_name}: VEGAN"
+    elif dish_ingredients <= set(VEGETARIAN):
+        return f"{dish_name}: VEGETARIAN"
+    elif dish_ingredients <= set(PALEO):
+        return f"{dish_name}: PALEO"
+    elif dish_ingredients <= set(KETO):
+        return f"{dish_name}: KETO"
+    else:
+        return f"{dish_name}: OMNIVORE"
+        
